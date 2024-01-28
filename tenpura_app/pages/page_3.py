9b66c44b0e783053cc6db8fe9d1a2a2f109bada3
junpi08171 <153448405+junpi08171@users.noreply.github.com>
2024-01-28 19:06:24 +0900

    Update page_3.py
    tenpura_app/

--- tenpura_app/pages/page_3.py
+++ tenpura_app/pages/page_3.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 
 
-df = pd.read_csv('./data/人口変化.csv', index_col='都道府県コード', encoding='shift_jis')
+df = pd.read_csv('tenpura_app/data/人口変化.csv', index_col='都道府県コード', encoding='shift_jis')
 st.dataframe(df)
